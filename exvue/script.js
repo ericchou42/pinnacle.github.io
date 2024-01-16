041139a7433af90dab651d6341ea6af12e85004a
ericchou <32100427+ericchou42@users.noreply.github.com>
2024-01-16 18:28:03 +0800

    Add files via upload

---
+++ exvue/script.js
@@ -0,0 +1,14 @@
+var mydata={
+  name: "吳哲宇",
+  message: "訊訊訊息訊息",
+  color: "black",
+  class: "",
+  books: ["哈利波特",
+      "波特萊爾大冒險",
+      "迷霧之子"
+  ]
+};
+var vm=new Vue({
+  el: "#app",
+  data: mydata
+});
