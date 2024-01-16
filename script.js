7513bb7cd67d376e867733df94dd2ecc863bcad7
ericchou <32100427+ericchou42@users.noreply.github.com>
2024-01-16 18:30:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,23 @@ function init(){
     toText.value=initTo;
     bodyText.value=initBody;
 }
+
+// alert() 
+function a() {
+  alert('這是彈跳警告視窗');
+}
+
+// confirm() 
+function c() {
+  m = confirm('這是彈跳確認視窗');
+  // 可透過真假值去做不同的變化
+  if(m == true)
+    alert("按下確定");
+}
+// prompt () 
+function p() {
+  prompt('這是輸入訊息視窗');
+  console.log(a);
+}
+
+
